0d8ff2befa99504a349a5865c54546e62a38cc70
elmyrockers <elmyrockers@gmail.com>
2026-05-10 01:29:23 +0800

    Update main function and its imports

--- grpc-servers/go-service/main.go
+++ grpc-servers/go-service/main.go
@@ -2,9 +2,33 @@ package main
 
 
 import (
-
+	"context"
+	"log"
+	
+	"net"
+	"google.golang.org/grpc"
+	"github.com/elmyrockers/grpc-crosslang-communication-demo/grpc-servers/go-service/pb/user"
 )
 
+
+
+
+
+
+
+
+
 func main(){
-	
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	user.RegisterUserServiceServer(grpcServer, &userServer{})
+
+	log.Println("gRPC server running on :50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
