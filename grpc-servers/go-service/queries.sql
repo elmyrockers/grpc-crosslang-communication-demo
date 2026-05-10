56ac86c3ef61b230726a296a7218d175073d1036
elmyrockers <elmyrockers@gmail.com>
2026-05-10 18:06:35 +0800

    Add a file for queries - All(), New(), Edit(), Delete()

---
+++ grpc-servers/go-service/queries.sql
@@ -0,0 +1,12 @@
+-- name: All :many
+SELECT id, name, age, location, email FROM users;
+
+-- name: New :exec
+INSERT INTO users (name, age, location, email)
+VALUES (?, ?, ?, ?);
+
+-- name: Edit :exec
+UPDATE users SET name = ?, age = ?, location = ?, email = ? WHERE id = ?;
+
+-- name: Delete :exec
+DELETE FROM users WHERE id = ?;
