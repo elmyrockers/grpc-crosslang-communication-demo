0f0a95e0f860bb5637f8e0b841aced4f45c4a2db
elmyrockers <elmyrockers@gmail.com>
2026-05-10 17:00:11 +0800

    Update main() - Set gRPC client as UserController attribute

--- http-server/main.go
+++ http-server/main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	// "fmt"
+	// "github.com/davecgh/go-spew/spew"
+
 	"log"
 	"github.com/elmyrockers/grpc-crosslang-communication-demo/http-server/controller"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/jet/v2"
+
+	"google.golang.org/grpc"
+	"github.com/elmyrockers/grpc-crosslang-communication-demo/http-server/pb/user"
 )
 
 func main() {
+	// Connect to go-service (gRPC server)
+		connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("did not connect: %v", err)
+		}
+		defer connection.Close()
+	
+	// Set gRPC Client as UserController attribute
+		client := user.NewUserServiceClient(connection)
+		userController := controller.UserController{ Client: client }
+
 	// Create app with Jet template engine
 		engine := jet.New("./views", ".jet")
 		app := fiber.New(fiber.Config{
@@ -19,7 +35,6 @@ func main() {
 		app.Get("/", func(c *fiber.Ctx) error {
 			return c.Redirect("/users")
 		})
-		userController := controller.UserController{}
 		app.Get("/users", userController.List )
 
 	// API Routes
