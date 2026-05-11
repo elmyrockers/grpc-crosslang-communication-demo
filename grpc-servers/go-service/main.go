9774bbffa1e2ebafcbf9d78ac69ef5682858c0c0
elmyrockers <elmyrockers@gmail.com>
2026-05-11 14:33:33 +0800

    Ensure the DB connection is closed at the correct time

--- grpc-servers/go-service/main.go
+++ grpc-servers/go-service/main.go
@@ -2,11 +2,17 @@ package main
 
 
 import (
-	_ "github.com/joho/godotenv"
+	"github.com/davecgh/go-spew/spew"
+
+	_ "github.com/joho/godotenv/autoload"
 	"context"
 	"log"
+	"os"
+	"fmt"
 	
 	"net"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc"
 	"github.com/elmyrockers/grpc-crosslang-communication-demo/grpc-servers/go-service/pb/user"
 	"database/sql"
@@ -20,24 +26,51 @@ type userServer struct {
 
 	query *db.Queries
 }
+
+func safeString(ns sql.NullString) string {
+	if ns.Valid { return ns.String }
+	return ""
+}
+
+
 func (s *userServer) All(ctx context.Context, req *user.GetRequest) (*user.GetResponse, error) {
-	users := []*user.User{
-		{Id: 1, Name: "Helmi Aziz", Age: 27, Location: "Kuala Lumpur", Email: "helmi@xeno.com.my"},
-		{Id: 2, Name: "Akmal Hazim", Age: 30, Location: "Alor Setar", Email: "hazim@gmail.com"},
+	dbUsers, err := s.query.All( context.Background() )
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to query users: %v", err)
 	}
-	return &user.GetResponse{Users: users}, nil
+
+	// Re-map dbUsers to pbUsers
+		pbUsers := make([]*user.User, 0, len(dbUsers))
+		for _, u := range dbUsers {
+			pbUsers = append(pbUsers, &user.User{
+				Id:    int32(u.ID),
+				Name:  u.Name,
+				Age:  int32(u.Age),
+				Location:  safeString(u.Location),
+				Email: safeString(u.Email),
+			})
+		}
+
+	return &user.GetResponse{Users: pbUsers}, nil
 }
 func (s *userServer) New(ctx context.Context, req *user.PostRequest) (*user.SuccessResponse, error) {
 	return &user.SuccessResponse{Success: true}, nil
 }
 
-func connectDB() *db.Queries {
+func connectDB() (*db.Queries, *sql.DB) {
+	// Load configs
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
+		dbUser := os.Getenv("DB_USER")
+		dbPass := os.Getenv("DB_PASS")
+		dbName := os.Getenv("DB_NAME")
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
 	// Open connection pool
-		sqlDB, err := sql.Open("mysql", "root:12345@tcp(localhost:3306)/grpc-demo")
+		sqlDB, err := sql.Open("mysql", dsn)
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer sqlDB.Close()
 
 	// Verify connection
 		if err = sqlDB.Ping(); err != nil {
@@ -45,13 +78,14 @@ func connectDB() *db.Queries {
 		}
 
 	// Wrap with sqlc, then return
-		return db.New(sqlDB)
+		return db.New(sqlDB), sqlDB
 }
 
 
 func main(){
 	// Connect to DB
-		query := connectDB()
+		query, sqlDB := connectDB()
+		defer sqlDB.Close()
 
 	// Listen to port 50051
 		lis, err := net.Listen("tcp", ":50051")
