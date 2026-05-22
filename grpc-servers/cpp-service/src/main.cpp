0ced2c2720686d51d2ac076bfa6663fe8f3b05d9
elmyrockers <elmyrockers@gmail.com>
2026-05-23 01:37:48 +0800

    Update main(): Register cpp-service and start grpc server

--- grpc-servers/cpp-service/src/main.cpp
+++ grpc-servers/cpp-service/src/main.cpp
@@ -1,9 +1,25 @@
+#include <string>
+#include <iostream>
+#include <grpcpp/grpcpp.h>
+
+
 import grpc.service;
 
 
-int main(){
-	UserServer user;
 
+
+
+int main(){
+	// Register service and listening port
+		UserServer userSv;
+		grpc::ServerBuilder builder;
+		builder.AddListeningPort( "0.0.0.0:50051", grpc::InsecureServerCredentials() );
+		builder.RegisterService( &userSv );
+
+	// Start server
+		std::cout << "\n\ngRPC server is listening on port 50051" << std::endl;
+		std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+		if (server) { server->Wait(); }
 
 	return 1;
 }
