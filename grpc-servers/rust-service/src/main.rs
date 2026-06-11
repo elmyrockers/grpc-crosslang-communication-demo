b4a67e204e8dd960bfa219ac219e7775a53c2307
elmyrockers <elmyrockers@gmail.com>
2026-06-11 19:46:41 +0800

    Update main entry point file

--- grpc-servers/rust-service/src/main.rs
+++ grpc-servers/rust-service/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hi");
+#[path = "fbs/user_generated.rs"]
+#[allow(warnings)]
+mod user_generated;
+
+mod codec;
+mod router;
+mod grpc_service;
+
+use std::sync::Arc;
+use grpc_service::UserService;
+use router::UserRouter;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    let addr = "[::1]:50052".parse()?;
+    tracing::info!("UserService listening on {}", addr);
+
+    tonic::transport::Server::builder()
+        .add_service(UserRouter(Arc::new(UserService::new())))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
