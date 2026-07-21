c8cbacc6c95a22ffea5f55f099ad95d9f123069f
elmyrockers <elmyrockers@gmail.com>
2026-07-21 18:06:53 +0800

    Add user_pb module, then connect rust service to go service

--- grpc-servers/rust-service/src/main.rs
+++ grpc-servers/rust-service/src/main.rs
@@ -1,10 +1,9 @@
 #[path = "fbs/user_generated.rs"]
 #[allow(warnings)]
 mod user_generated;
-// pub mod pb {
-//     include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/pb/user.rs"));
-// }
-
+#[path = "pb/user.rs"]
+#[allow(warnings)]
+mod user_pb;
 mod codec;
 mod router;
 mod grpc_service;
@@ -12,16 +11,23 @@ mod grpc_service;
 use std::sync::Arc;
 use grpc_service::UserService;
 use router::UserRouter;
+use user_pb::user_service_client::UserServiceClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
+    // Connect to the Go gRPC UserService
+        let client = UserServiceClient::connect("http://[::1]:50053").await?;
+
+
+
+
     let addr = "[::1]:50052".parse()?;
     tracing::info!("UserService listening on {}", addr);
 
     tonic::transport::Server::builder()
-        .add_service(UserRouter(Arc::new(UserService::new())))
+        .add_service(UserRouter(Arc::new(UserService::new(client))))
         .serve(addr)
         .await?;
 
